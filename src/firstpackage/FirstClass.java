b767d7de24c3d0e842d5a8adb2ed9228f7d6eb33
hozkan007 <67195015+hozkan007@users.noreply.github.com>
2020-06-20 21:44:43 +0200

    Update FirstClass.java
    artık çözüm arıyoruz

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -6,7 +6,7 @@ public class FirstClass {
 		
 		System.out.println("first commit from local repository");
 		System.out.println("second commit from local repository");
-		System.out.println("conflict yapmak istiyoruz");
+		System.out.println("we are against the conflict");
 		System.out.println("first commit from remote repository");
 		
 
